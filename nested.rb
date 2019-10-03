388a9de25c190b3f697fe3c299d61bf5c3b2e9ba
Brandon Furr <brandon.furr123@gmail.com>
2019-10-03 22:12:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -16,11 +16,11 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
 	
 	programmer_hash = 
  		{
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +56,18 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-	programmer_hash = 
- 		{
+  
+hash = {:yukihiro_matsumoto => {
+	  :known_for => "Ruby",
+	    :languages => ["LISP", "C"]
+}
+}
+	    
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,8 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
+      programmer_hash = hash
+      
 
-    
 end
 
 def changing_alan
@@ -106,6 +109,7 @@ def changing_alan
 end
 
 def adding_to_dennis
+  hash = ["Assembly"]
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -121,5 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+      programmer_hash[:dennis_ritchie][:languages] = hash
+
 
 end
